d50f6303292325d42f739a2fa281d81319bd1d35
andrewsharp <jasharpluevano@gmail.com>
2024-07-16 15:08:01 -0400

    finished headr

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,7 +1,7 @@
 use clap::{Arg, Command};
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use std::error::Error;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -18,57 +18,83 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
     }
 }
 
-
 pub fn get_args() -> Result<Args, Box<dyn Error>> {
     let matches = Command::new("headr")
-            .version("0.1.0")
-            .author("J. Andrew Sharp Luevano <jasharpluevano@gmail.com>")
-            .about("head written in Rust")
-            .arg(
-                Arg::new("lines")
-                    .short('n')
-                    .long("lines")
-                    .value_name("LINES")
-                    .help("Number of lines")
-                    .value_parser(clap::value_parser!(u64).range(1..))
-                    .default_value("10"),
-            )
-            .arg(
-                Arg::new("bytes")
-                    .short('c')
-                    .long("bytes")
-                    .value_name("BYTES")
-                    .help("Number of bytes")
-                    .conflicts_with("lines")
-                    .value_parser(clap::value_parser!(u64).range(1..)),
-            )
-            .arg(
-                Arg::new("files")
-                    .value_name("FILE")
-                    .help("Input file(s)")
-                    .num_args(1..)
-                    .default_value("-"),
-            )
-            .get_matches();
+        .version("0.1.0")
+        .author("J. Andrew Sharp Luevano <jasharpluevano@gmail.com>")
+        .about("head written in Rust")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .value_parser(clap::value_parser!(u64).range(1..))
+                .default_value("10"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .conflicts_with("lines")
+                .value_parser(clap::value_parser!(u64).range(1..)),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(1..)
+                .default_value("-"),
+        )
+        .get_matches();
 
-    Ok(
-        Args {
-            files: matches
-                .get_many("files")
-                .expect("files required")
-                .cloned()
-                .collect(),
-            lines: matches.get_one("lines").cloned().unwrap(),
-            bytes: matches.get_one("bytes").cloned(),
-        }
-    )
+    Ok(Args {
+        files: matches
+            .get_many("files")
+            .expect("files required")
+            .cloned()
+            .collect(),
+        lines: matches.get_one("lines").cloned().unwrap(),
+        bytes: matches.get_one("bytes").cloned(),
+    })
 }
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let num_files = args.files.len();
+    let mut file_num = 0;
     for filename in args.files {
         match open(&filename) {
-             
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+
+                if num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        &filename
+                    );
+                }
+
+                if let Some(num_bytes) = args.bytes {
+                    let mut buffer = vec![0; num_bytes as usize];
+                    let bytes_read = file.read(&mut buffer)?;
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..args.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes  == 0 {
+                            break;
+                        }
+                        print!("{line}");
+                        line.clear();
+                    }
+                }
+            }
         }
+        file_num = file_num + 1;
     }
     Ok(())
 }
